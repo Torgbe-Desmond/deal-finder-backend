640a799d2ce337818119a84084c18d61975fc518
Torgbe-Desmond <123torgbe@gmail.com>
2025-03-31 15:04:42 +0000

    adding a hit route for uptimeRobot

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import SuccessResponseObject from "./utils/successResponseObject.js";
 import getSearchResults from "./utils/getSearchResults.js";
 import cors from "cors";
 import express from "express";
+import recursiveScrapeWebsite from "./scrappers/recursiveScrapeWebsite.js";
 const app = express();
 // http://localhost:3000/","https://deal-finder-orcin.vercel.app
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://deal-finder-orcin.vercel.app", // Or replace with specific origin: "http://your-frontend.com"
+    origin: "*",
     credentials: true,
     methods: ["POST", "GET"],
     allowedHeaders: ["Content-Type"],
@@ -20,7 +21,7 @@ app.post("/search", async (req, res) => {
   const { category, query } = req.query;
   console.log("query", query.toLocaleLowerCase());
   console.log("category", category.toLocaleLowerCase());
-  const selectedScrapers = req.body.selectedScrapers
+  const selectedScrapers = req.body.selectedScrapers;
   try {
     const response = await getSearchResults(
       query.toLocaleLowerCase(),
@@ -38,10 +39,11 @@ app.post("/search", async (req, res) => {
   }
 });
 
-app.get("/recursive-search", async (req, res) => {
+app.post("/recursive-search", async (req, res) => {
   const { link, domain } = req.body;
 
   try {
+    const response = await recursiveScrapeWebsite(domain, link);
     const responsObject = new SuccessResponseObject(
       response,
       "Search results",
@@ -51,6 +53,10 @@ app.get("/recursive-search", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+app.get("", async (req, res) => {
+  res.send("I love to hit you hard daddy");
 });
 
 app.listen(PORT, () => {
